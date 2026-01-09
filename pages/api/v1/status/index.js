d1cbc3fad5c8913aa4a6cd76f195fc9ffba69143
Bianco Da Costa Oliveira <49594278+ocnaibill@users.noreply.github.com>
2026-01-09 18:44:51 -0300

    adiciona prevenção contra vazamentos sql e informações da db na rota /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "Tudo Certo :)" });
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const versionValue = versionResult.rows[0].server_version;
+
+  const openConectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const openConections = openConectionsResult.rows.length;
+  console.log(openConections);
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionValue,
+        max_connections: parseInt(maxConnections),
+        open_connections: openConections,
+      },
+    },
+  });
 }
 
 export default status;
