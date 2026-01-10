c8b206bd0b8cded2068f01b804dfef0c23f607be
ocnaibill <biancocosta01@gmail.com>
2026-01-10 11:59:13 -0300

    finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,22 +6,24 @@ async function status(request, response) {
   const versionResult = await database.query("SHOW server_version;");
   const versionValue = versionResult.rows[0].server_version;
 
-  const openConectionsResult = await database.query(
-    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
-  const openConections = openConectionsResult.rows.length;
-  console.log(openConections);
-
   const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openConectionsResult = await database.query(
+    {
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName]
+    });
+  const openConectionsValue = openConectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: versionValue,
-        max_connections: parseInt(maxConnections),
-        open_connections: openConections,
+        max_connections: parseInt(maxConnectionsValue),
+        open_connections: openConectionsValue,
       },
     },
   });
